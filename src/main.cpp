e3cff3734e7a98bdeb49abb3382f1e47443b4589
JoseFG05 <jfgracol@gmail.com>
2025-04-08 01:59:52 -0400

    perf: actualizacion main

--- src/main.cpp
+++ src/main.cpp
@@ -8,34 +8,61 @@ int main() {
     Tree<string> clanTree;
     clanTree.buildTreeFromCSV("data.csv");
     clanTree.readContribuyentesFromCSV("contribuyentes.csv");
-
-    cout << "Arbol en PreOrder:" << endl;
-    clanTree.preOrder(clanTree.getFounder());
-
+    int op;
     Node<Clan<string>, List<Contribuyentes<string>>>* liderActual = clanTree.buscarLider(clanTree.getFounder());
+
     if(liderActual) {
         cout << "\nLider Actual:" << endl;
         liderActual->getData().print();
     } else {
         cout << "\nNo se encontro lider." << endl;
+        return 0;
     }
 
-    cout << "\nModificacion de Nodo:" << endl;
-    clanTree.modificarDatosNodo();
+    do{
+        clanTree.actualizarLider();
+        cout<<"Menu:\n1. Modificar nodo.\n2. Imprimir nodo.\n3. Imprimir arbol.\n4. Mostrar sucesion.\n5. Imprimir lider.\n0. Salir.\n";
+        cin>>op;
+        switch(op){
+            case 1:{
+                clanTree.modificarDatosNodo();
+                cin.get();
+            }
+            break;
+            case 2:{
+                cout<<"Ingresa el Id del nodo a imprimir\n";
+                string id;
+                cin>>id;
+                cout<<"\n";
+                clanTree.findNode(clanTree.getFounder(),id)->getData().print();
+                clanTree.findNode(clanTree.getFounder(),id)->getList().print();
+                cin.get();
+            }
+            break;
+            case 3:{
+                clanTree.preOrder(clanTree.getFounder());
+                cin.get();
+            }
+            break;
+            case 4:{
+            }
+            break;
+            case 5:{
+                cout<<"Lider actual:\n";
+                liderActual = clanTree.buscarLider(clanTree.getFounder());
+                liderActual->getData().print();
+                cin.get();
+            }
+            break;
+            case 0:
+            cout<<"Saliendo.\n";
+            cin.get();
+            break;
+            default:
+            cout<<"Opcion invalida.\n";    
+        }
 
-    cout << "\nActualizando Lider..." << endl;
-    clanTree.actualizarLider();
-
-    cout << "\nArbol Actualizado:" << endl;
-    clanTree.preOrder(clanTree.getFounder());
-
-    liderActual = clanTree.buscarLider(clanTree.getFounder());
-    if(liderActual) {
-        cout << "\nLider Actual:" << endl;
-        liderActual->getData().print();
-    } else {
-        cout << "\nNo se encontro lider." << endl;
-    }
+    }while(op!=0);
 
     return 0;
 }
